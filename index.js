6bd5851a3d7a809361156d87257778e42eef896a
Alyssa Jane <xcthaeh@gmail.com>
2019-06-10 16:58:24 -0400

    created db, added a migrations table, started endpoints(get requests)

--- index.js
+++ index.js
@@ -8,6 +8,41 @@ const port = 8000;
 
 const server = express();
 
+
+server.get('/api/me', protected, (req, res) => {
+  db('users')
+  .select('id', 'username', 'password')
+  .where({ id: req.session.user })
+  .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+
+
+server.get('/api/users', protected, (req, res) => {
+    db('users')
+    .select('id', 'username', 'password')
+    .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+});
+
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.send('Error occurred. Could not log out.');
+      } else {
+        res.send('Hope to see you back soon!');
+      }
+    });
+  } 
+});
+
+
 server.listen(port, () => {
   console.log(`Server is running on ... ${port}`);
 });
